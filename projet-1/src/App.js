9d65c89e30e59ac829d10274bc434dd4215d9f44
Yas <ben.yachou@yahoo.com>
2022-09-27 13:28:22 +0200

    ADD: Titre dynamique

--- projet-1/src/App.js
+++ projet-1/src/App.js
@@ -10,6 +10,24 @@ class App extends Component {
     color: 'green'
   }
 
+  changeTitle = (e) => {
+    console.log(e.target);
+    this.setState({ titre: 'Nouveau titre' });
+  
+  }
+
+  changeViaParam= (titre) => {
+    this.setState({ titre: titre });
+  }
+
+  changeViaBind = (param) => {
+    this.setState({ titre: param });
+  }
+
+  modifTitre = (e) => {
+    this.setState({ titre: e.target.value });
+  }
+
   render() {
       return (
 
@@ -20,7 +38,11 @@ class App extends Component {
         title = {this.state.titre}
       />
 
-      <button>Changer le nom en dur</button>
+      <button onClick={this.changeTitle}>Changer le nom en dur</button>
+      <button onClick={ () => this.changeViaParam('Titre via un parametre')}>un autre titre</button>
+      <button onClick={ this.changeViaBind.bind(this, 'wesh la famille')}>un autre</button>
+      <input type="text" onChange= {this.modifTitre} value={this.state.titre} />
+
       </div>
     );
   }
